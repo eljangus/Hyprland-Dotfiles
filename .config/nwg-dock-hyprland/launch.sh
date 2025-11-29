5934d2229e3e471e5486ebacec6024c8cfd9eff4
eljangus <eljangus@gmail.com>
2025-11-29 18:27:45 +0100

    regular update

--- .config/nwg-dock-hyprland/launch.sh
+++ .config/nwg-dock-hyprland/launch.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-nwg-dock-hyprland -l overlay -mb 10 -lp 'start' -w 10 -c 'rofi -show drun' -i 48 -iw '-98' -o DP-3 -d -hd 20 -ico "/home/$USER/.config/nwg-dock-hyprland/logo2.png"
+nwg-dock-hyprland -l overlay -mb 10 -lp 'start' -w 10 -c 'rofi -show drun' -i 48 -iw '-98' -o DP-3 -x -hd 20 -ico "/home/$USER/.config/nwg-dock-hyprland/logo2.png"
