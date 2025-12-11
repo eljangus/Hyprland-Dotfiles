18cc5d6cb9f8168de0ae475692e234035edafb26
eljangus <eljangus@gmail.com>
2025-12-11 17:53:51 +0100

    random colors

--- .config/RofiScripts/WallpaperChanger/wall.sh
+++ .config/RofiScripts/WallpaperChanger/wall.sh
@@ -1,1 +1,1 @@
-/home/elias/.config/ThemeSwitcher/Themes/ColorThemes/Gruvbox-Dark/wall.sh
+/home/elias/.config/ThemeSwitcher/Themes/ColorThemes/Nightfox/wall.sh
--- .config/RofiScripts/WallpaperChanger/wallrandom.sh
+++ .config/RofiScripts/WallpaperChanger/wallrandom.sh
@@ -1,1 +1,1 @@
-/home/elias/.config/ThemeSwitcher/Themes/ColorThemes/Gruvbox-Dark/wallrandom.sh
+/home/elias/.config/ThemeSwitcher/Themes/ColorThemes/Nightfox/wallrandom.sh
