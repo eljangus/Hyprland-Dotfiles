9437f275f02af8ff4c7d24fc95ce3ec79a4dfe20
eljangus <eljangus@gmail.com>
2025-12-13 23:32:39 +0100

    update

--- .config/nwg-dock-hyprland/launch.sh
+++ .config/nwg-dock-hyprland/launch.sh
@@ -2,4 +2,4 @@
 
 killall nwg-dock-hyprland
 sleep 0.3s
-nwg-dock-hyprland -l overlay -mb 0 -lp 'start' -w 10 -c 'rofi -show drun' -i 48 -iw '-98' -o DP-3 -d -hd 20 -ico "/home/$USER/.config/nwg-dock-hyprland/logo2.png"
+nwg-dock-hyprland -l overlay -mb 0 -lp 'start' -w 10 -c 'rofi -show drun' -i 48 -iw '-98' -o DP-3 -x -hd 20 -ico "/home/$USER/.config/nwg-dock-hyprland/logo2.png"
