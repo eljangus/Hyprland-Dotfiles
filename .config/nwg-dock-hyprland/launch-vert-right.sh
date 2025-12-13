6683d0310cc8a8e6f96b079b9af092ad6f5fb002
eljangus <eljangus@gmail.com>
2025-12-13 17:49:43 +0100

    update

--- .config/nwg-dock-hyprland/launch-vert-right.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-killall nwg-dock-hyprland
-sleep 0.3s
-nwg-dock-hyprland -l overlay -mr 10 -lp 'start' -w 10 -c 'rofi -show drun' -i 48 -iw '-98' -o DP-3 -d -hd 20 -ico "/home/$USER/.config/nwg-dock-hyprland/logo2.png" -p right
