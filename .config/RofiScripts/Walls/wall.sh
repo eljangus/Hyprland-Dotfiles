47d0030001679ae2b2e91d43fdc3d2cdc102308e
eljangus <eljangus@gmail.com>
2025-12-27 17:34:24 +0100

    fixing important waybar bug in regards of scripts. added 'setsid' before each script to swaync/waybar to make sure they run independently

--- .config/RofiScripts/Walls/wall.sh
+++ .config/RofiScripts/Walls/wall.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 DIR="$HOME/.config/RofiScripts/Walls/Walls"
-
 selected=$(ls "$DIR" | while read -r A; do
-    echo -en "$A\x00icon\x1f$DIR/$A\n"
+echo -en "$A\x00icon\x1f$DIR/$A\n"
 done | rofi -dmenu -i -m DP-3 -config '~/.config/RofiScripts/WallpaperChanger/WC.rasi')
-
 [ -z "$selected" ] && exit 0
-
 matugen image "$DIR/$selected" -m dark -t scheme-tonal-spot
 ln -sfn "$DIR/$selected" ~/.config/RofiScripts/Walls/Wall
 ln -sfn "$DIR/$selected" ~/.config/RofiScripts/WallpaperChanger/Wall
