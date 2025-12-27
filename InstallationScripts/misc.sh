3a965cf5a65a2d76d873f842972ae9f5736acc12
eljangus <eljangus@gmail.com>
2025-12-27 17:56:39 +0100

    added .local script

--- InstallationScripts/misc.sh
+++ InstallationScripts/misc.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
 
-rm ~/.config/kwalletrc
-rm ~/.config/mimeapps.list
-sleep 0.1s
 cp ~/Hyprland-Dotfiles/.config/kwalletrc ~/.config
 cp ~/Hyprland-Dotfiles/.config/mimeapps.list ~/.config
+cp ~/Hyprland-Dotfiles/.local/share/applications/* ~/.local/share/applications/
